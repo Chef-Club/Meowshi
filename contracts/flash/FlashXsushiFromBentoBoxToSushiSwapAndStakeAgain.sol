20ce52a654d4a0aa80c1df7668e488d12654ab1e
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-08-13 20:25:10 -0400

    Create FlashXsushiFromBentoBoxToSushiSwapAndStakeAgain.sol

---
+++ contracts/flash/FlashXsushiFromBentoBoxToSushiSwapAndStakeAgain.sol
@@ -0,0 +1,285 @@
+// optimizer: 4289999999
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.6.12;
+
+// File @boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol@v1.2.0
+// License-Identifier: MIT
+
+/// @notice A library for performing overflow-/underflow-safe math,
+/// updated with awesomeness from of DappHub (https://github.com/dapphub/ds-math).
+library BoringMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require((c = a + b) >= b, "BoringMath: Add Overflow");
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require((c = a - b) <= a, "BoringMath: Underflow");
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b == 0 || (c = a * b) / b == a, "BoringMath: Mul Overflow");
+    }
+
+    function to128(uint256 a) internal pure returns (uint128 c) {
+        require(a <= uint128(-1), "BoringMath: uint128 Overflow");
+        c = uint128(a);
+    }
+
+    function to64(uint256 a) internal pure returns (uint64 c) {
+        require(a <= uint64(-1), "BoringMath: uint64 Overflow");
+        c = uint64(a);
+    }
+
+    function to32(uint256 a) internal pure returns (uint32 c) {
+        require(a <= uint32(-1), "BoringMath: uint32 Overflow");
+        c = uint32(a);
+    }
+}
+
+/// @notice A library for performing overflow-/underflow-safe addition and subtraction on uint128.
+library BoringMath128 {
+    function add(uint128 a, uint128 b) internal pure returns (uint128 c) {
+        require((c = a + b) >= b, "BoringMath: Add Overflow");
+    }
+
+    function sub(uint128 a, uint128 b) internal pure returns (uint128 c) {
+        require((c = a - b) <= a, "BoringMath: Underflow");
+    }
+}
+
+/// @notice A library for performing overflow-/underflow-safe addition and subtraction on uint64.
+library BoringMath64 {
+    function add(uint64 a, uint64 b) internal pure returns (uint64 c) {
+        require((c = a + b) >= b, "BoringMath: Add Overflow");
+    }
+
+    function sub(uint64 a, uint64 b) internal pure returns (uint64 c) {
+        require((c = a - b) <= a, "BoringMath: Underflow");
+    }
+}
+
+/// @notice A library for performing overflow-/underflow-safe addition and subtraction on uint32.
+library BoringMath32 {
+    function add(uint32 a, uint32 b) internal pure returns (uint32 c) {
+        require((c = a + b) >= b, "BoringMath: Add Overflow");
+    }
+
+    function sub(uint32 a, uint32 b) internal pure returns (uint32 c) {
+        require((c = a - b) <= a, "BoringMath: Underflow");
+    }
+}
+
+// File @boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol@v1.2.0
+// License-Identifier: MIT
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /// @notice EIP 2612
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
+
+// File @boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol@v1.2.0
+// License-Identifier: MIT
+
+// solhint-disable avoid-low-level-calls
+
+library BoringERC20 {
+    bytes4 private constant SIG_SYMBOL = 0x95d89b41; // symbol()
+    bytes4 private constant SIG_NAME = 0x06fdde03; // name()
+    bytes4 private constant SIG_DECIMALS = 0x313ce567; // decimals()
+    bytes4 private constant SIG_BALANCE_OF = 0x70a08231; // balanceOf(address)
+    bytes4 private constant SIG_TRANSFER = 0xa9059cbb; // transfer(address,uint256)
+    bytes4 private constant SIG_TRANSFER_FROM = 0x23b872dd; // transferFrom(address,address,uint256)
+
+    function returnDataToString(bytes memory data) internal pure returns (string memory) {
+        if (data.length >= 64) {
+            return abi.decode(data, (string));
+        } else if (data.length == 32) {
+            uint8 i = 0;
+            while(i < 32 && data[i] != 0) {
+                i++;
+            }
+            bytes memory bytesArray = new bytes(i);
+            for (i = 0; i < 32 && data[i] != 0; i++) {
+                bytesArray[i] = data[i];
+            }
+            return string(bytesArray);
+        } else {
+            return "???";
+        }
+    }
+
+    /// @notice Provides a safe ERC20.symbol version which returns '???' as fallback string.
+    /// @param token The address of the ERC-20 token contract.
+    /// @return (string) Token symbol.
+    function safeSymbol(IERC20 token) internal view returns (string memory) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(SIG_SYMBOL));
+        return success ? returnDataToString(data) : "???";
+    }
+
+    /// @notice Provides a safe ERC20.name version which returns '???' as fallback string.
+    /// @param token The address of the ERC-20 token contract.
+    /// @return (string) Token name.
+    function safeName(IERC20 token) internal view returns (string memory) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(SIG_NAME));
+        return success ? returnDataToString(data) : "???";
+    }
+
+    /// @notice Provides a safe ERC20.decimals version which returns '18' as fallback value.
+    /// @param token The address of the ERC-20 token contract.
+    /// @return (uint8) Token decimals.
+    function safeDecimals(IERC20 token) internal view returns (uint8) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(SIG_DECIMALS));
+        return success && data.length == 32 ? abi.decode(data, (uint8)) : 18;
+    }
+    
+    /// @notice Provides a gas-optimized balance check to avoid a redundant extcodesize check in addition to the returndatasize check.
+    /// @param token The address of the ERC-20 token.
+    /// @param to The address of the user to check.
+    /// @return amount The token amount.
+    function safeBalanceOf(IERC20 token, address to) internal view returns (uint256 amount) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(SIG_BALANCE_OF, to));
+        require(success && data.length >= 32, "BoringERC20: BalanceOf failed");
+        amount = abi.decode(data, (uint256));
+    } 
+
+    /// @notice Provides a safe ERC20.transfer version for different ERC-20 implementations.
+    /// Reverts on a failed transfer.
+    /// @param token The address of the ERC-20 token.
+    /// @param to Transfer tokens to.
+    /// @param amount The token amount.
+    function safeTransfer(
+        IERC20 token,
+        address to,
+        uint256 amount
+    ) internal {
+        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(SIG_TRANSFER, to, amount));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "BoringERC20: Transfer failed");
+    }
+
+    /// @notice Provides a safe ERC20.transferFrom version for different ERC-20 implementations.
+    /// Reverts on a failed transfer.
+    /// @param token The address of the ERC-20 token.
+    /// @param from Transfer tokens from.
+    /// @param to Transfer tokens to.
+    /// @param amount The token amount.
+    function safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(SIG_TRANSFER_FROM, from, to, amount));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "BoringERC20: TransferFrom failed");
+    }
+}
+
+interface IFlashBorrower {
+    /// @notice The flashloan callback. `amount` + `fee` needs to repayed to msg.sender before this call returns.
+    /// @param sender The address of the invoker of this flashloan.
+    /// @param token The address of the token that is loaned.
+    /// @param amount of the `token` that is loaned.
+    /// @param fee The fee that needs to be paid on top for this loan. Needs to be the same as `token`.
+    /// @param data Additional data that was passed to the flashloan function.
+    function onFlashLoan(
+        address sender,
+        IERC20 token,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata data
+    ) external;
+}
+
+interface IBatchFlashBorrower {
+    /// @notice The callback for batched flashloans. Every amount + fee needs to repayed to msg.sender before this call returns.
+    /// @param sender The address of the invoker of this flashloan.
+    /// @param tokens Array of addresses for ERC-20 tokens that is loaned.
+    /// @param amounts A one-to-one map to `tokens` that is loaned.
+    /// @param fees A one-to-one map to `tokens` that needs to be paid on top for each loan. Needs to be the same token.
+    /// @param data Additional data that was passed to the flashloan function.
+    function onBatchFlashLoan(
+        address sender,
+        IERC20[] calldata tokens,
+        uint256[] calldata amounts,
+        uint256[] calldata fees,
+        bytes calldata data
+    ) external;
+}
+
+/// @notice Interface for SushiSwap.
+interface ISushiSwap {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+}
+
+/// @notice Interface for depositing into SushiBar.
+interface ISushiBarEnter { 
+    function enter(uint256 amount) external;
+}
+
+contract FlashXsushiFromBentoBoxToSushiSwapAndStakeAgain is IFlashBorrower {
+    using BoringMath for uint256;
+    using BoringERC20 for IERC20;
+    
+    IERC20 constant wETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); // ETH wrapper contract v9 (mainnet)
+    IERC20 constant sushiToken = IERC20(0x6B3595068778DD592e39A122f4f5a5cF09C90fE2); // SUSHI token contract (mainnet)
+    address constant sushiBar = 0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272; // xSUSHI staking contract for SUSHI (mainnet)
+    ISushiSwap constant sushiSwapSushiETHpair = ISushiSwap(0x795065dCc9f64b5614C407a6EFDC400DA6221FB0); // SUSHI/ETH pair on SushiSwap (mainnet)
+    ISushiSwap constant sushiSwapXsushiETHpair = ISushiSwap(0x36e2FCCCc59e5747Ff63a03ea2e5C0c2C14911e7); // xSUSHI/ETH pair on SushiSwap (mainnet)
+ 
+    constructor() public {
+        sushiToken.approve(sushiBar, type(uint256).max); // max approve xSUSHI staking from this contract
+    }
+    
+    function onFlashLoan(
+        address sender, // account that activates flash loan from BENTO
+        IERC20, // default to flash borrow xSUSHI
+        uint256 amount, // xSUSHI amount flash borrowed
+        uint256 fee, // BENTO flash loan fee
+        bytes calldata // default to not use data in flash loan
+    ) external override {
+        /// @dev The following internal functions run xSUSHI flash loan strategy (swap/swap/stake).
+        swapFlashxSushiForWeth(amount); // swap borrowed xSUSHI for wETH
+        swapWethForSushi(); // swap output wETH for SUSHI
+        ISushiBarEnter(sushiBar).enter(sushiToken.safeBalanceOf(address(this))); // stake output SUSHI into xSUSHI
+        /// @dev The following functions pay back xSUSHI to BENTO with fee and send any winnings to `sender`.
+        uint256 payback = amount.add(fee); // calculate `payback` to BENTO as borrowed xSUSHI `amount` + `fee`
+        IERC20(sushiBar).safeTransfer(msg.sender, payback); // send `payback` to BENTO
+        IERC20(sushiBar).safeTransfer(sender, IERC20(sushiBar).safeBalanceOf(address(this)).sub(payback)); // skim remainder xSUSHI winnings to `sender`
+    }
+    
+    function swapFlashxSushiForWeth(uint256 amount) private {
+        (uint256 reserve0, uint256 reserve1, ) = sushiSwapXsushiETHpair.getReserves();
+        uint256 amountInWithFee = amount.mul(997);
+        uint256 out = amountInWithFee.mul(reserve0) / reserve1.mul(1000).add(amountInWithFee);
+        sushiSwapXsushiETHpair.swap(0, out, address(this), "");
+    }
+    
+    function swapWethForSushi() private {
+        uint256 wETHamount = wETH.safeBalanceOf(address(this));
+        (uint256 reserve0, uint256 reserve1, ) = sushiSwapSushiETHpair.getReserves();
+        uint256 amountInWithFee = wETHamount.mul(997);
+        uint256 out = amountInWithFee.mul(reserve0) / reserve1.mul(1000).add(amountInWithFee);
+        wETH.safeTransfer(address(sushiSwapSushiETHpair), wETHamount);
+        sushiSwapSushiETHpair.swap(out, 0, address(this), "");
+    }
+}
